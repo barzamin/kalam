e024cb2446c4c42b1bda60bcacfadc85069fcc21
erin petra sofiya moon <erin@hecke.rs>
2023-03-10 18:53:26 -0600

    sketching out a system to build an AE plugin

---
+++ ae/kalam.ts
@@ -0,0 +1,8 @@
+const fname: string = File.openDialog('select klm file', 'Kalam files:*.klm,All files:*.*');
+
+setTimeout(() => {
+    alert("foo")
+}, 200);
+
+import {FOO} from './common.ts';
+console.log(FOO);
---
+++ ae/spack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: {
+        'kalam': __dirname + '/kalam.ts',
+    },
+    output: {
+        path: __dirname + '/dist',
+    },
+};
